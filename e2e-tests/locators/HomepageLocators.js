02b71ce9446552195ea70f7ee20b562fe4b5cde2
Craig Greenhalgh <craig@greenhalgh.me>
2016-10-26 21:00:57 +0100

    deleted

--- e2e-tests/locators/HomepageLocators.js
+++
@@ -1,5 +0,0 @@
-var HomePageLocators = function() {
-    this.yourName= element(by.model('yourName'));
-    this.outcome = element(by.binding('yourName'));
-}
-module.exports = new HomePageLocators();
